af6b0aed47c0b07b494b3ce0042b4997212e3f59
the hengker <40335023+uciharis@users.noreply.github.com>
2024-01-23 20:17:51 +0700

    Update app.js
    belajar event change dan input

--- prevent-default/app.js
+++ prevent-default/app.js
@@ -2,6 +2,17 @@ const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const list = document.querySelector('#notes');
 
+// input.addEventListener('change', (e)=>{
+//     console.log('ada perubahan');
+// });
+
+input.addEventListener('input', (e)=>{
+    document.querySelector('h1').innerText = input.value;
+    console.log('ada input');
+});
+
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const noteValue = input.value;
