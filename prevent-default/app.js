20eae822d7fbf4ab7837ee9e278a415afe0dab12
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-01-24 20:21:45 +0700

    Update app.js
    belajar event change

--- prevent-default/app.js
+++ prevent-default/app.js
@@ -2,6 +2,11 @@ const form = document.querySelector('#form');
 const input = document.querySelector('input');
 const list = document.querySelector('#notes');
 
+input.addEventListener('change', (e)=>{
+    console.log('nilai berubah');
+});
+
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     const noteValue = input.value;
