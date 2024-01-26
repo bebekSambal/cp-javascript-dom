17734473a7fe3ded16fc9a98278fb2f48db62224
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-01-26 21:48:45 +0700

    Update app.js
    fungsi yang mendisable tombol kedua player apabila salah satu sudah mencapai skor 5

--- papan-skor/app.js
+++ papan-skor/app.js
@@ -5,8 +5,27 @@ const p2Display = document.querySelector('#p2-display');
 
 
 let p1Score = 0;
+let p2Score = 0;
+
+const win = 5;
+let isGameOver = false;
+
 p1Button.addEventListener('click',()=>{
-    
-    p1Score += 1;
-    p1Display.textContent = p1Score;
+    if(!isGameOver){
+        p1Score += 1;
+        if(p1Score === win){
+            isGameOver = true;
+        }
+        p1Display.textContent = p1Score;
+    }
+});
+
+p2Button.addEventListener('click',()=>{
+    if(!isGameOver){
+        p2Score += 1;
+        if(p2Score === win){
+            isGameOver = true;
+        }
+        p2Display.textContent = p2Score;
+    }
 });
