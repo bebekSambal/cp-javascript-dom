b7078b8309f36a6bb3b91bbafca50a7e84ee05cb
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-01-28 01:28:06 +0700

    final touch
    sudah jadi, menambahkan fungsi reset dan dropdown skorwin point

--- papan-skor/app.js
+++ papan-skor/app.js
@@ -2,13 +2,22 @@ const p1Button = document.querySelector('#p1-button');
 const p2Button = document.querySelector('#p2-button');
 const p1Display = document.querySelector('#p1-display');
 const p2Display = document.querySelector('#p2-display');
-
+const resetButton = document.querySelector('#reset');
+const winpointOpsi = document.querySelector('#opsi');
 
 let p1Score = 0;
 let p2Score = 0;
 
-const win = 5;
+//const win = 5;
 let isGameOver = false;
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+}
 
 p1Button.addEventListener('click',()=>{
     if(!isGameOver){
@@ -29,3 +38,10 @@ p2Button.addEventListener('click',()=>{
         p2Display.textContent = p2Score;
     }
 });
+
+resetButton.addEventListener('click',reset);
+
+winpointOpsi.addEventListener('change', function(){
+    win = parseInt(winpointOpsi.value); // atau pake parseInt(this.value)
+    reset();
+})
